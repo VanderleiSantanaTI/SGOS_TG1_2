85a532a47f58a89491404047a568de3cac939fda
Vanderlei Santana <vanderleisantanati@gmail.com>
2025-10-01 02:55:54 -0300

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   
   // API Configuration
-  apiUrl: 'https://api.sgos.com/api/v1',
+  // apiUrl: 'https://api.sgos.com/api/v1',
+  apiUrl: 'http://localhost:8000/api/v1',
   apiTimeout: 30000, // 30 segundos
   
   // App Configuration
